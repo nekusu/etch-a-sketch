ebf6a7702732297734966c77698a197054628aaa
nekusu <nosoynahuel11@gmail.com>
2021-08-05 01:37:36 -0300

    Add eraser mode and grid toggle

--- js/etchASketch.js
+++ js/etchASketch.js
@@ -1,11 +1,14 @@
 const grid = document.querySelector('#grid');
 const colorPickers = document.querySelectorAll('.color-picker');
-const modeButtons = document.querySelectorAll('.button');
+const modeButtons = document.querySelectorAll('.button.mode');
 const sizeSlider = document.querySelector('#size-slider');
 const sizeSpans = document.querySelectorAll('.size');
+const gridToggle = document.querySelector('#grid-toggle');
+const clearButton = document.querySelector('#clear');
 let isDown = false;
 let paintMode = 'color';
 let colors = { background: colorPickers[0].value, fill: colorPickers[1].value };
+let gridLines = true;
 let lastTile;
 updateGrid();
 
@@ -35,10 +38,11 @@ function updateGrid() {
 	grid.style.grid = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
 
 	for (let i = 0; i < size ** 2; i++) {
-		const div = document.createElement('div');
-		div.style.backgroundColor = colors.background;
-		div.style.border = '1px solid gray';
-		grid.appendChild(div);
+		const tile = document.createElement('div');
+		tile.classList.add('tile');
+		tile.style.backgroundColor = colors.background;
+		tile.style.border = '1px solid ' + (gridLines ? 'gray' : 'transparent');
+		grid.appendChild(tile);
 	}
 }
 
@@ -60,14 +64,32 @@ function paint(e) {
 			case 'shading':
 				tile.style.backgroundColor = modifyColor(tileRgb, -20);
 				break;
+			case 'eraser':
+				tile.style.backgroundColor = colors.background;
 		}
 		lastTile = tile;
+	}
+}
+
+function toggleGridLines() {
+	gridLines = !gridLines;
+	gridToggle.firstElementChild.innerHTML = gridLines ? 'grid_on' : 'grid_off';
+	for (let tile of grid.children) {
+		tile.style.border = '1px solid ' + (gridLines ? 'gray' : 'transparent');
+	}
+}
+
+function clearGrid() {
+	for (let tile of grid.children) {
+		tile.style.backgroundColor = colors.background;
 	}
 }
 
 colorPickers.forEach(picker => picker.addEventListener('change', selectColor));
 modeButtons.forEach(button => button.addEventListener('click', selectMode))
 sizeSlider.addEventListener('change', updateGrid);
+gridToggle.addEventListener('click', toggleGridLines);
+clearButton.addEventListener('click', clearGrid);
 grid.addEventListener('mousedown', e => {
 	e.preventDefault();
 	isDown = true;
